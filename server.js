73cac95633a5c6f6ecf929354778cf9aefcc3ab8
Nathan Hughes <nhug@deakin.edu.au>
2023-12-03 08:37:06 +1100

    Code and site is working

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const cardList = [
+	{
+		title: "Kitten 2",
+		image: "images/kitten2.png",
+		link: "About Kitten 2",
+		desciption: "Meow meow meow meow meow!"
+	},
+	{
+		title: "Kitten 3",
+		image: "images/kitten3.png",
+		link: "About Kitten 3",
+		desciption: "Meow meow meow meow meow meow meow meow meow meow!"
+	}
+]
+
 app.get('/api/projects',(req,res) => {
 	res.json({statusCode: 200, data: cardList, message:"Success"})
 	})
